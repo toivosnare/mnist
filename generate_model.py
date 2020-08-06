afe712766850032fbdbb3512f31cd287a4f7308d
Toivo Snåre <topisn@gmail.com>
2020-08-06 15:13:50 +0300

    Normalize training data to the test_model.py format

--- generate_model.py
+++ generate_model.py
@@ -2,14 +2,15 @@ import tensorflow as tf
 
 SIZE = 28
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train = x_train.reshape(-1, SIZE * SIZE) / 255.0
-x_test = x_test.reshape(-1, SIZE * SIZE) / 255.0
+x_train = 1 - x_train.reshape(-1, SIZE * SIZE) / 255.0
+x_test = 1 - x_test.reshape(-1, SIZE * SIZE) / 255.0
 
 model = tf.keras.models.Sequential([
   tf.keras.Input(shape=(SIZE * SIZE,)),
   tf.keras.layers.Dense(128, activation='relu',),
   tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+  tf.keras.layers.Dense(10),
+  tf.keras.layers.Softmax()
 ])
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
